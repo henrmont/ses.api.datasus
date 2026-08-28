1ca3dfa5a1f405959c22999352513c72c4083d8c
Jorge Monteiro <dev.henrmont@gmail.com>
2026-08-28 14:57:47 -0400

    (hml) final

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api', Auth::class])
+    ->prefix('datasus')
     ->controller(SigtapController::class)
     ->group(function () {
         Route::post('process', 'process');
