9787b84057323a7b4ca5cff1e7a4d76992dad958
Jorge Monteiro <dev.henrmont@gmail.com>
2026-09-24 14:19:30 -0400

    (fix) datasus users

--- database/migrations/2026_09_24_145311_create_professional_types_table.php
+++ database/migrations/2026_09_24_145311_create_professional_types_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('professionals', function (Blueprint $table) {
+        Schema::create('professional_types', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
-            $table->string('name');
-            $table->string('cns');
-            $table->string('registration');
-            $table->softDeletes();
+            $table->foreignId('professional_id');
+            $table->string('type');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('professionals');
+        Schema::dropIfExists('professional_types');
     }
 };
